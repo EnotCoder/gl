45ea6755f629caab708c6af5f1f67526ac970edb
EnotCoder <samaks.sasha.com>
2025-11-14 20:55:01 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+GLfloat vert[4][3] = {
+    {0.1,0.1,0},
+    {0.1,-0.1,0},
+    {-0.1,-0.1,0},
+    {-0.1,0.1,0}
+};
+
 int main() {
     if (!glfwInit()) {
         std::cerr << "Ошибка инициализации GLFW" << std::endl;
@@ -8,7 +15,7 @@ int main() {
     }
 
     // Создаем окно 800x600 с заголовком "Тест GLFW"
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Тест GLFW", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Тест GLFW", nullptr, nullptr);
     if (!window) {
         std::cerr << "Ошибка создания окна" << std::endl;
         glfwTerminate();
@@ -20,9 +27,19 @@ int main() {
     // Главный цикл приложения
     while (!glfwWindowShouldClose(window)) {
         // Очистка экрана с цветом (черный)
-        glClearColor(1.0f, 0.1f, 0.5f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_QUADS);
+        glColor3f(1,0,0);
+        glVertex3fv(vert[0]);
+        glColor3f(0,1,0);
+        glVertex3fv(vert[1]);
+        glColor3f(0,0,1);
+        glVertex3fv(vert[2]);
+        glColor3f(1,1,1);
+        glVertex3fv(vert[3]);
+        glEnd();
         // Обработка событий
         glfwPollEvents();
 
