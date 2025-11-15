8235cd542b261db0cd47428c012ee1f06fb744f5
EnotCoder <samaks.sasha.com>
2025-11-15 21:20:18 +0300

    gl ++ commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+#define GLFW_INCLUDE_NONE
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <sstream>
+#include <fstream>
 #include <iostream>
+
+using namespace std;
 
 GLfloat vert[4][3] = {
     {0.1,0.1,0},
@@ -8,13 +14,13 @@ GLfloat vert[4][3] = {
     {-0.1,0.1,0}
 };
 
+
 int main() {
     if (!glfwInit()) {
         std::cerr << "Ошибка инициализации GLFW" << std::endl;
         return -1;
     }
 
-    // Создаем окно 800x600 с заголовком "Тест GLFW"
     GLFWwindow* window = glfwCreateWindow(800, 800, "Тест GLFW", nullptr, nullptr);
     if (!window) {
         std::cerr << "Ошибка создания окна" << std::endl;
@@ -22,7 +28,28 @@ int main() {
         return -1;
     }
 
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(window);  // Контекст должен быть сделан текущим ДО glewInit()
+
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Ошибка инициализации GLEW" << std::endl;
+        return -1;
+    }
+
+    GLuint VAO;
+    glGenVertexArrays(1,&VAO);
+    glBindVertexArray(VAO);
+
+    GLuint VBO;
+    glGenBuffers(1,&VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vert), vert, GL_STATIC_DRAW);
+
+
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER,0);
 
     // Главный цикл приложения
     while (!glfwWindowShouldClose(window)) {
@@ -30,16 +57,10 @@ int main() {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_POLYGON);
-        glColor3f(1,0,0);
-        glVertex3fv(vert[0]);
-        glColor3f(0,1,0);
-        glVertex3fv(vert[1]);
-        glColor3f(0,0,1);
-        glVertex3fv(vert[2]);
-        glColor3f(1,1,1);
-        glVertex3fv(vert[3]);
-        glEnd();
+        
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLE_FAN,0,4);
+        glBindVertexArray(0);
 
         // Обработка событий
         glfwPollEvents();
