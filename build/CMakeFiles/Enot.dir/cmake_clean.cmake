37bd96b633b6341b6721fba787c1bb861c4d0017
EnotCoder <samaks.sasha@gmail.com>
2025-11-22 20:06:28 +0300

    Сохранение изменений перед добавлением subtree

---
+++ build/CMakeFiles/Enot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Enot.dir/main.cpp.o"
+  "CMakeFiles/Enot.dir/main.cpp.o.d"
+  "Enot"
+  "Enot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Enot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Enot.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Enot.
+# This may be replaced when dependencies are built.
